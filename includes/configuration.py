7a3756032c19e692a6033db98990c9d453a38d60
Adrian Salinero Lobo <adrian.salinerolobo@rwth-aachen.de>
2023-02-09 11:52:52 +0100

    first commit

---
+++ includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formulaunodatalake/raw"
+processed_folder_path = "/mnt/formulaunodatalake/processed"
+presentation_folder_path = "/mnt/formulaunodatalake/presentation"
+
+# COMMAND ----------
+
